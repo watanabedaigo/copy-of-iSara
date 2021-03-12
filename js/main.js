010577a67dd7133fff93a267f855a06e8b0abfad
watanabedaigo <wdaigm0801@gmail.com>
2021-03-12 18:16:42 +0900

    design about

--- js/main.js
+++ js/main.js
@@ -30,3 +30,55 @@ const recruitments = new Vue({
         ],
     },
 })
+
+Vue.component('skill-component',{
+    props:['skill'],
+    template:`
+        <div class="col-4" v-bind:id="skill.id">
+            <figure>
+                <img v-bind:src="skill.img" v-bind:alt="skill.alt">
+                <figcaption v-html="skill.figcaption"></figcaption>  
+            </figure>
+        </div>
+    `
+})
+
+const skills = new Vue({
+    el:'#about',
+    data:{
+        skills:[
+            {
+                id:'skill',
+                img:'img/skill.jpg',
+                alt:'プログラミングスキル',
+                figcaption:'基礎的な<br>プログラミングスキル',
+            },
+            {
+                id:'sales',
+                img:'img/sales.jpg',
+                alt:'営業力',
+                figcaption:'案件獲得に必要な<br>営業力',
+            },
+            {
+                id:'document',
+                img:'img/document.jpg',
+                alt:'一連の知識',
+                figcaption:'見積もり作成から<br>納品までの知識',
+            },
+            {
+                id:'plusone',
+                img:'img/plusone.jpg',
+                alt:'付加価値',
+                figcaption:'自分の付加価値を<br>高めるスキ',
+            },
+            {
+                id:'connection',
+                img:'img/connection.jpg',
+                alt:'横のつながり',
+                figcaption:'フリーランス同士の<br>横のつながり',
+            },
+
+
+        ],
+    },
+})
